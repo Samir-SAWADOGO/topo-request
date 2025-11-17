d2b8b3974ccdd3a815d105ce085925ee56f4360a
Samir-SAWADOGO <sawadogosamir307@gmail.com>
2025-11-17 13:00:36 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+function login() {
+    window.location.href = "dashboard.html";
+}
+
+function logout() {
+    window.location.href = "index.html";
+}
+
+function sendRequest() {
+    alert("Demande envoyée (simulation). Firebase arrive ! 🚀");
+}
+
+window.onload = () => {
+    // future Firebase loading here
+};
